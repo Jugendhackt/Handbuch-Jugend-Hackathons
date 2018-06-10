4a61ffa21b163f0722e024df14e2857da13004d8
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-10 12:35:43 +0200

    Remove testing timeout

--- js/preloader.js
+++ js/preloader.js
@@ -1,9 +1,6 @@
 
 $(document).ready(function() {
 	
-	setTimeout(function() {
-		console.log("Disabling preloader...")
-		$('body').addClass('loaded');
-	}, 3000);
+	$('body').addClass('loaded');
 	
 });
